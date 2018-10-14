2a4e3bd015f08abcb0f4556c923f45c45a48f123
Stephanie French <french5033@gmail.com>
2018-10-14 15:02:01 -0700

    update

--- Desktop/charactergame.js
+++ Desktop/charactergame.js
@@ -10,41 +10,9 @@ String character = [mage, fighter, traveler, alien]
 String abilities = [magicka, health, strength, agility, stealth, accuracy]
 // String ability1 = "magicka" // String ability2 = "health" // String ability3 = "strength" // String ability4 = "agility" // String ability5 = "stealth"
 
-//The way you want to do this - create an object for your character which should have the following properties:
-//race ex. lizard //class ex. mage //age ex. 15
-//ability1 (name this whatever you chose as your ability)
-//ability2 (name this whatever you chose as your ability)
-//ability3 (name this whatever you chose as your ability)
-//ability4 (name this whatever you chose as your ability)
-//Then to create the values for these items in your object do the following:
-
 // For race and class, choose a random item from your list (see my CodePen for a hint)
 race = random
 class = random
 // For the age property, assign a randomly generated number for age of your character (say between 10 and 50).
 integer age = Math.random(10, 50)
-// Randomly assign values for all 4 of your abilities. These should all be done separately. (see my CodePen for a hint)
-// Output detailed character information to the screen.
-// Then ask at least 2 questions for a storyline similar to our Choose Your Own Adventure game.
 
-// Before you start coding do the following on paper:
-// You will use CSS to style your character generator - colors, images, fonts that will work with the type of game environment your character would be seen in. Feel free to add some complexity to the character generator - like a name generator as well (hint: save values to 2 arrays and use a similar random array generator with a concatenation)
-// The way you want to do this - create an object for your character which should have the following properties and any others that you wish to add:
-//
-// race
-// class
-// age
-// ability1 (name this whatever you chose as your ability)
-// ability2 (name this whatever you chose as your ability)
-// ability3 (name this whatever you chose as your ability)
-// ability4 (name this whatever you chose as your ability)
-// ability5 (name this whatever you chose as your ability)
-// ability6 (name this whatever you chose as your ability)
-// Then to create the values for these items in your object do the following:
-
-// For race and class, choose a random item from your list (see my CodePen for a hint)
-// For the age property, assign a randomly generated number for age of your character (say between 0 and 100).
-// Randomly assign values for all 6 of your abilities. These should all be done separately. (see my CodePen for a hint)
-// Output the character information to the screen in a creative way.
-// Then ask at least 4 questions for a storyline similar to our Choose Your Own Adventure game.
-// Bonus XP for those who include an image based on the character as well!
